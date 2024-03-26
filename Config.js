de79388cc5ad6c23930372aa316025512bca7db8
23Tsuma <144899827+23Tsuma@users.noreply.github.com>
2024-03-26 15:48:56 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['+254 793 867761']
+global.ownername = "IT Expert Leon"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
